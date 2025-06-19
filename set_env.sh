c203900651dc3e208d119ce8ac2dd31021bf97ef
Marie Pauline Sauvant <mp.sauvant@gmail.com>
2025-06-19 10:34:49 -0500

    Change folder structure for visual clarity

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/etc/secret-volume/gcp-hopeful-buckeye-sa.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/secrets/gcp-hopeful-buckeye-sa.json"
 echo "Environment variable GOOGLE_APPLICATION_CREDENTIALS set."
