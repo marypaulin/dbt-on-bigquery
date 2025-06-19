e64b602e1d9649cf07126a4430328d82751d7f9f
Marie Pauline Sauvant <mp.sauvant@gmail.com>
2025-06-19 15:32:57 -0500

    Add Python script to extract data from BigQuery

---
+++ scripts/extract_data.py
@@ -0,0 +1,53 @@
+# Extraxt data from BigQuery and save it to Parquet files
+# Run this script from root to read .env file and interpret data path correctly
+
+import logging
+import os
+
+from dotenv import load_dotenv
+from google.cloud import bigquery
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+# Load environment variables
+load_dotenv()
+logging.info("Environment variables loaded")
+
+DATASET_ID = os.getenv("BIGQUERY_DATASET")
+if not DATASET_ID:
+    logging.error("BIGQUERY_DATASET environment variable not set")
+    exit(1)
+
+DATA_PATH = os.getenv("DATA_PATH")
+if not DATA_PATH:
+    logging.error("DATA_PATH environment variable not set")
+    exit(1)
+
+# Create BigQuery client
+client = bigquery.Client()
+logging.info("BigQuery client created")
+
+# Save BigQuery dataset tables to local Parquet files
+try:
+    dataset = client.get_dataset(DATASET_ID)
+    logging.info("Dataset ID: %s", dataset.dataset_id)
+    logging.info("Location: %s", dataset.location)
+    logging.info("Downloading tables to Parquet files...")
+    tables = list(client.list_tables(dataset))
+    for table_item in tables:
+        table = client.get_table(table_item.reference)
+        mb_size = table.num_bytes / 1024 ** 2
+        # Download table to DataFrame
+        df = client.list_rows(table).to_dataframe()
+        # Save DataFrame to Parquet file
+        parquet_file = DATA_PATH + f"{table.table_id}.parquet"
+        df.to_parquet(parquet_file, index=False)
+        logging.info("Saved %s (%i rows, %.2f MB)", table.table_id, table.num_rows, mb_size)
+except Exception as e:
+    logging.error("Failed to extract dataset: %s", e)
+    exit(1)
--- set_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/secrets/gcp-hopeful-buckeye-sa.json"
-echo "Environment variable GOOGLE_APPLICATION_CREDENTIALS set."
